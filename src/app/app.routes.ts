f5d4a9e2228fa6c0db053c91933778a34aefcaa7
baesparza <bruno.be81@gmail.com>
2017-06-09 20:52:38 -0500

    bug fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,9 @@ import { FotosComponent } from './components/fotos/fotos.component';
 import { CargaComponent } from './components/carga/carga.component';
 
 const ROUTES: Routes = [
-    { path: '**', redirectTo: 'fotos' },
     { path: 'fotos', component: FotosComponent },
     { path: 'carga', component: CargaComponent },
+    { path: '**', redirectTo: 'fotos' }
 ];
 
 export const ROUTER = RouterModule.forRoot(ROUTES);
