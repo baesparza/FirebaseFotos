57886c1f426280dbe7003833c41976a6409f248b
baesparza <bruno.be81@gmail.com>
2017-06-09 20:34:47 -0500

    carga component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CargaComponent } from './components/carga/carga.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CargaComponent
   ],
   imports: [
     BrowserModule,
