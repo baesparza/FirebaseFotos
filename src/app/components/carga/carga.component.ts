ec8a3563cd01be6e55187bb095e03eb97d4b8330
baesparza <bruno.be81@gmail.com>
2017-06-09 22:49:12 -0500

    layout, basic functions

--- src/app/components/carga/carga.component.ts
+++ src/app/components/carga/carga.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { CargaimagenesService } from './../../services/cargaimagenes.service';
+
+import { FileItem } from './../../directives/file-item';
 
 @Component({
   selector: 'app-carga',
   templateUrl: './carga.component.html',
   styles: []
 })
-export class CargaComponent implements OnInit {
+export class CargaComponent {
 
-  constructor() { }
+  estaSobreDropzone: boolean = false;
+  permiteCargar: boolean = true;
 
-  ngOnInit() {
+  archivos: FileItem[] = [];
+
+  constructor(public _cargaimagenesService: CargaimagenesService) { }
+
+  cargarFirebase () {
+    this.permiteCargar = false;
+    this._cargaimagenesService.uploadImagenes(this.archivos);
   }
 
+  limpiarArchivos () {
+    this.archivos = [];
+    this.permiteCargar = true;
+  }
 }
