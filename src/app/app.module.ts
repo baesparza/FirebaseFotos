d6b485ec27776e0d08f2c8c1eb2149dbf9d191d1
baesparza <bruno.be81@gmail.com>
2017-06-09 22:11:06 -0500

    obtener ultimas imagenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 // Router
 import { ROUTER } from './app.routes';
 
 // Configuracion
 import { firebaseConfig } from './../config/firebase.config';
+
+// Services
+import { CargaimagenesService } from './services/cargaimagenes.service';
 
 import { AppComponent } from './app.component';
 import { CargaComponent } from './components/carga/carga.component';
@@ -25,9 +29,12 @@ import { FotosComponent } from './components/fotos/fotos.component';
     FormsModule,
     HttpModule,
     ROUTER,
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
-  providers: [],
+  providers: [
+    CargaimagenesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/cargaimagenes.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { FileItem } from './../directives/file-item';
+
+import * as firebase from 'firebase';
+
+@Injectable()
+export class CargaimagenesService {
+
+  private Carpeta_Imagenes: string = 'img';
+
+  constructor(public _angularFireDatabase: AngularFireDatabase) { }
+
+  listaUltimasImagenes (maxImg: number): FirebaseListObservable<any[]> {
+
+    return this._angularFireDatabase.list(`${ this.Carpeta_Imagenes }`, {
+      query: {
+        limitToLast: maxImg
+      }
+    });
+  }
+
+}
